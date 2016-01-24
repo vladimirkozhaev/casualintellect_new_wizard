66bfae6e90b5be4ae4ae80c5f611d906b8560174
vkozhaev <vkozhaev@gmail.com>
2016-01-24 23:36:39 +0200

    Add casualintellect formatter

--- org.casualintellect.editor/src-gen/org/casualintellect/AbstractCasualIntellectRuntimeModule.java
+++ org.casualintellect.editor/src-gen/org/casualintellect/AbstractCasualIntellectRuntimeModule.java
@@ -7,6 +7,7 @@ import com.google.inject.Binder;
 import com.google.inject.Provider;
 import com.google.inject.name.Names;
 import java.util.Properties;
+import org.casualintellect.formatting2.CasualIntellectFormatter;
 import org.casualintellect.generator.CasualIntellectGenerator;
 import org.casualintellect.parser.antlr.CasualIntellectAntlrTokenFileProvider;
 import org.casualintellect.parser.antlr.CasualIntellectParser;
@@ -18,6 +19,9 @@ import org.casualintellect.services.CasualIntellectGrammarAccess;
 import org.casualintellect.validation.CasualIntellectValidator;
 import org.eclipse.xtext.Constants;
 import org.eclipse.xtext.IGrammarAccess;
+import org.eclipse.xtext.formatting2.FormatterPreferenceValuesProvider;
+import org.eclipse.xtext.formatting2.FormatterPreferences;
+import org.eclipse.xtext.formatting2.IFormatter2;
 import org.eclipse.xtext.generator.IGenerator2;
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
@@ -30,6 +34,7 @@ import org.eclipse.xtext.parser.antlr.ITokenDefProvider;
 import org.eclipse.xtext.parser.antlr.Lexer;
 import org.eclipse.xtext.parser.antlr.LexerBindings;
 import org.eclipse.xtext.parser.antlr.LexerProvider;
+import org.eclipse.xtext.preferences.IPreferenceValuesProvider;
 import org.eclipse.xtext.resource.IContainer;
 import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.resource.containers.IAllContainersState;
@@ -189,6 +194,16 @@ public abstract class AbstractCasualIntellectRuntimeModule extends DefaultRuntim
 	// contributed by org.eclipse.xtext.xtext.generator.generator.GeneratorFragment2
 	public Class<? extends IGenerator2> bindIGenerator2() {
 		return CasualIntellectGenerator.class;
+	}
+	
+	// contributed by org.eclipse.xtext.xtext.generator.formatting.Formatter2Fragment2
+	public Class<? extends IFormatter2> bindIFormatter2() {
+		return CasualIntellectFormatter.class;
+	}
+	
+	// contributed by org.eclipse.xtext.xtext.generator.formatting.Formatter2Fragment2
+	public void configureFormatterPreferences(Binder binder) {
+		binder.bind(IPreferenceValuesProvider.class).annotatedWith(FormatterPreferences.class).to(FormatterPreferenceValuesProvider.class);
 	}
 	
 }
